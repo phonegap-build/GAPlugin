43b098f7167b476d33516ad3a683923ac1c8d55c
Levi Morales <levi.morales@nogginlabs.com>
2013-08-21 16:18:33 -0500

    updated to new plugin spec

--- www/GAPlugin.js
+++ www/GAPlugin.js
@@ -6,100 +6,86 @@
  * Copyright (c) 2011, IBM Corporation
  */
  
-var cordova = require('cordova');
 
-cordova.define("cordova/plugin/GAPlugin",
-    
-    function (require, exports, module) {
+var exec = require("cordova/exec");
 
-        var exec = require("cordova/exec");
+/**
+ * Constructor.
+ *
+ * @returns {GAPlugin}
+ */
+var GAPlugin = { };
 
-        /**
-         * Constructor.
-         *
-         * @returns {GAPlugin}
-         */
-        function GAPlugin() { }
+GAPlugin.available = false;
 
-        GAPlugin.prototype.available = false;
+// initialize google analytics with an account ID and the min number of seconds between posting
+//
+// id = the GA account ID of the form 'UA-00000000-0'
+// period = the minimum interval for transmitting tracking events if any exist in the queue
+GAPlugin.init = function(success, fail, id, period)
+{
+    success = success || GAPlugin.onSuccess;
+    fail = fail || GAPlugin.onFail;
+    return exec(success, fail, 'GAPlugin', 'initGA', [id, period]);
+};
 
-        GAPlugin.prototype.testGA = function(success, fail)
-        {
-            success = success || this.onSuccess;
-            fail = fail || this.onFail;
-            return exec(success, fail, 'GAPlugin', 'test', []);
-        };
-
-         // initialize google analytics with an account ID and the min number of seconds between posting
-        //
-        // id = the GA account ID of the form 'UA-00000000-0'
-        // period = the minimum interval for transmitting tracking events if any exist in the queue
-        GAPlugin.prototype.init = function(success, fail, id, period)
-        {
-            success = success || this.onSuccess;
-            fail = fail || this.onFail;
-            return exec(success, fail, 'GAPlugin', 'initGA', [id, period]);
-        };
-
-        // log an event
-        //
-        // category = The event category. This parameter is required to be non-empty.
-        // eventAction = The event action. This parameter is required to be non-empty.
-        // eventLabel = The event label. This parameter may be a blank string to indicate no label.
-        // eventValue = The event value. This parameter may be -1 to indicate no value.
-        GAPlugin.prototype.trackEvent = function(success, fail, category, eventAction, eventLabel, eventValue)
-        {
-            success = success || this.onSuccess;
-            fail = fail || this.onFail;
-            return exec(success, fail, 'GAPlugin', 'trackEvent', [category, eventAction, eventLabel, eventValue]);
-        };
+// log an event
+//
+// category = The event category. This parameter is required to be non-empty.
+// eventAction = The event action. This parameter is required to be non-empty.
+// eventLabel = The event label. This parameter may be a blank string to indicate no label.
+// eventValue = The event value. This parameter may be -1 to indicate no value.
+GAPlugin.trackEvent = function(success, fail, category, eventAction, eventLabel, eventValue)
+{
+    success = success || GAPlugin.onSuccess;
+    fail = fail || GAPlugin.onFail;
+    return exec(success, fail, 'GAPlugin', 'trackEvent', [category, eventAction, eventLabel, eventValue]);
+};
 
 
-        // log a page view
-        //
-        // pageURL = the URL of the page view
-        GAPlugin.prototype.trackPage = function(success, fail, pageURL)
-        {
-            success = success || this.onSuccess;
-            fail = fail || this.onFail;
-            return exec(success, fail, 'GAPlugin', 'trackPage', [pageURL]);
-        };
+// log a page view
+//
+// pageURL = the URL of the page view
+GAPlugin.trackPage = function(success, fail, pageURL)
+{
+    success = success || GAPlugin.onSuccess;
+    fail = fail || GAPlugin.onFail;
+    return exec(success, fail, 'GAPlugin', 'trackPage', [pageURL]);
+};
 
-        // Set a custom variable. The variable set is included with
-        // the next event only. If there is an existing custom variable at the specified
-        // index, it will be overwritten by this one.
-        //
-        // value = the value of the variable you are logging
-        // index = the numerical index of the dimension to which this variable will be assigned (1 - 20)
-        //  Standard accounts support up to 20 custom dimensions.
-        GAPlugin.prototype.setVariable = function(success, fail, index, value)
-        {
-            success = success || this.onSuccess;
-            fail = fail || this.onFail;
-            return exec(success, fail, 'GAPlugin', 'setVariable', [index, value]);
-        };
-        
-        GAPlugin.prototype.exit = function(success, fail)
-        {
-            success = success || this.onSuccess;
-            fail = fail || this.onFail;
-            return exec(success, fail, 'GAPlugin', 'exitGA', []);
-        };
+// Set a custom variable. The variable set is included with
+// the next event only. If there is an existing custom variable at the specified
+// index, it will be overwritten by this one.
+//
+// value = the value of the variable you are logging
+// index = the numerical index of the dimension to which this variable will be assigned (1 - 20)
+//  Standard accounts support up to 20 custom dimensions.
+GAPlugin.setVariable = function(success, fail, index, value)
+{
+    success = success || GAPlugin.onSuccess;
+    fail = fail || GAPlugin.onFail;
+    return exec(success, fail, 'GAPlugin', 'setVariable', [index, value]);
+};
 
-        GAPlugin.onSuccess = function (data)
-        {
-            console.log('GAPlugin::onSuccess');
-            if (data)
-                console.log(data);
-        };
+GAPlugin.exit = function(success, fail)
+{
+    success = success || GAPlugin.onSuccess;
+    fail = fail || GAPlugin.onFail;
+    return exec(success, fail, 'GAPlugin', 'exitGA', []);
+};
 
-        GAPlugin.onFail = function (data)
-        {
-            console.log('GAPlugin::onFail');
-            if (data)
-                console.log(data);
-        };
+GAPlugin.onSuccess = function (data)
+{
+    console.log('GAPlugin::onSuccess');
+    if (data)
+        console.log(data);
+};
 
-        var gaPlugin = new GAPlugin();
-        module.exports = gaPlugin;
-    });
+GAPlugin.onFail = function (data)
+{
+    console.log('GAPlugin::onFail');
+    if (data)
+        console.log(data);
+};
+
+module.exports = GAPlugin;
